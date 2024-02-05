72664fedf733468db543c2aca61430d6497f10ef
r.volshchukov <r.volshchukov@seven-sky.net>
2024-02-05 12:18:06 +0300

    add observer in state for render and delete render file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { rerenderTree } from './render';
+import {addNote, subscribe, updateTextArea} from './redux/state';
+import { BrowserRouter  } from 'react-router-dom';
 import state from './redux/state';
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
+
+export let rerenderTree = (state) => {
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App state={state} addNote = {addNote} updateTextArea = {updateTextArea}/>
+    </BrowserRouter>
+  </React.StrictMode>
+);
+}
 
 rerenderTree(state);
+subscribe(rerenderTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
